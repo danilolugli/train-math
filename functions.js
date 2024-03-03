66f491115d06d39079d88e12e7145503bc2696c4
Danilo Lugli <daniloluglisantos@gmail.com>
2024-03-02 21:13:35 -0300

    Arrumando interface

--- functions.js
+++ functions.js
@@ -5,13 +5,17 @@ function gerarNumerosAleatorios(a1, a2) {
     return lista;
 }
 
-function buscarNumeros(){
+function buscarNumeros() {
     var labelN1 = document.querySelector("#n1");
     var labelN2 = document.querySelector("#n2");
     var numsAleatorios = gerarNumerosAleatorios();
         
     labelN1.textContent = numsAleatorios[0];
     labelN2.textContent = numsAleatorios[1];
+
+    document.querySelector('#resposta').style.display = "flex";
+    document.querySelector('#botaoResponder').style.display = "block";
+    document.querySelector('.calculo').style.display = "flex"; 
 
     return numsAleatorios;
 } 
@@ -70,29 +74,36 @@ function verificaResultado() {
     console.log(operacao);  
     var resposta = document.getElementById("resposta").value; //para pegar input do usuario tem q ser value
 
-    if(operacao == "+"){
+    if (operacao == "+"){
         resultado = n1 + n2;
     }
-    else if(operacao == "–"){
+    else if (operacao == "–"){
         resultado = n1 - n2;
     }
-    else if(operacao == "×"){
+    else if (operacao == "×"){
         resultado = n1 * n2;
     }
-    else if(operacao == "÷"){
-        if(n2 == 0){
+    else if (operacao == "÷"){
+        if (n2 == 0){
             alert("Impossível dividir por ZERO! Gere a conta novamente por favor.");
         }
-        else{
+        else {
           resultado = n1 / n2;
         }
     }
 
-    if(resposta == resultado){
-        alert("PARABÉNS, ACERTOU!");
+    if (resposta == resultado){
+        alert("ACERTOU!!! Treine com outra operação :D");
+
+        document.querySelector('.calculo').style.display = "none";
+        document.querySelector('#resposta').style.display = "none";
+
+        document.querySelector('#resposta').value = "";
+        document.querySelector('#botaoResponder').style.display = "none";
     }
-    else{
-        alert("ERROU!");
+    else {
+        alert("ERROU! :("); 
+        document.querySelector('#resposta').value = "";
     }
 }
 
